3caa7c898172afb8910b6729355be42dd82937bd
douglaswcastro <douglaswcastro@hotmail.com>
2018-03-31 21:40:59 -0300

    criação geral dos forms

--- ControleMedicoFamiliar/CadastroUsuario.Designer.cs
+++ ControleMedicoFamiliar/CadastroUsuario.Designer.cs
@@ -53,7 +53,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label2.Location = new System.Drawing.Point(239, 55);
+            this.label2.Location = new System.Drawing.Point(244, 87);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(55, 20);
             this.label2.TabIndex = 1;
@@ -63,7 +63,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label3.Location = new System.Drawing.Point(239, 101);
+            this.label3.Location = new System.Drawing.Point(244, 133);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(64, 20);
             this.label3.TabIndex = 2;
@@ -73,7 +73,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label4.Location = new System.Drawing.Point(239, 147);
+            this.label4.Location = new System.Drawing.Point(244, 179);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(56, 20);
             this.label4.TabIndex = 3;
@@ -81,21 +81,21 @@
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(239, 78);
+            this.txtNome.Location = new System.Drawing.Point(244, 110);
             this.txtNome.Name = "txtNome";
             this.txtNome.Size = new System.Drawing.Size(241, 20);
             this.txtNome.TabIndex = 4;
             // 
             // txtUsuario
             // 
-            this.txtUsuario.Location = new System.Drawing.Point(239, 124);
+            this.txtUsuario.Location = new System.Drawing.Point(244, 156);
             this.txtUsuario.Name = "txtUsuario";
             this.txtUsuario.Size = new System.Drawing.Size(241, 20);
             this.txtUsuario.TabIndex = 5;
             // 
             // txtSenha
             // 
-            this.txtSenha.Location = new System.Drawing.Point(239, 170);
+            this.txtSenha.Location = new System.Drawing.Point(244, 202);
             this.txtSenha.Name = "txtSenha";
             this.txtSenha.Size = new System.Drawing.Size(241, 20);
             this.txtSenha.TabIndex = 6;
@@ -103,7 +103,7 @@
             // btnCadastrar
             // 
             this.btnCadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnCadastrar.Location = new System.Drawing.Point(308, 206);
+            this.btnCadastrar.Location = new System.Drawing.Point(313, 238);
             this.btnCadastrar.Name = "btnCadastrar";
             this.btnCadastrar.Size = new System.Drawing.Size(113, 23);
             this.btnCadastrar.TabIndex = 7;
